43c2204f0f288d62a251c9c1d1d48143ef27242f
Vanderson Pereira <vandoap@hotmail.com>
2018-10-20 10:21:22 -0300

    Added a character
    - New examples about objects. \n - Added a character in game project.

--- jogo_da_velha/scripts/script.js
+++ jogo_da_velha/scripts/script.js
@@ -2,33 +2,33 @@ alert('Conectado');
 
 var field;
 
-function Field(cols, rows, containerId){
-
-    // Propriedades
-    this.cols = cols;
-    this.rows = rows;
-    this.container =  document.querySelector(containerId);
+class Field{
+   constructor (cols, rows, containerId){
+        // Propriedades
+        this.cols = cols;
+        this.rows = rows;
+        this.container =  document.querySelector(containerId);
+        this.createField();
+   }
 
     // Métodos
-    this.createField = function(){
+    createField(){
         var field = [];
         for (let i = 0; i < this.rows; i++) {
             field[i] = [];
-
             for (let j = 0; j < this.cols; j++) {
                 field[i].push(this.createRock());
             }
         }
-
         this.field = field;
         this.drawField();
     }
 
-    this.createRock = function(){
+    createRock(){
         return Math.trunc(Math.random() * 5) === 1 ? '@' : '';
     }
 
-    this.drawField = function(){
+    drawField(){
         var template = '';
 
         for (let i = 0; i < this.rows; i++) {
@@ -38,11 +38,30 @@ function Field(cols, rows, containerId){
             }
             template += '</tr>';
         }
-        console.log(template);
         this.container.innerHTML = template;
+    }
+}
+
+class Character{
+    constructor(field, x, y, face){
+        this.table = field;
+        this.x = x;
+        this.y = y;
+        this.face = face;
+        this.setPosition(this.x, this.y);
+    }
+
+    setPosition(x, y){
+        if(this.table.field[y][x] === ''){
+            this.table.field[this.y][this.x] = '';
+            this.x = x;
+            this.y = y;
+            this.table.field[this.y][this.x] = this.face;
+            this.table.drawField();
+        }
     }
 }
 
 // Forçando as entradas
 field = new Field(3, 4, '#myTable');
-field.createField();
+new Character(field, 2, 1, '-,-');
